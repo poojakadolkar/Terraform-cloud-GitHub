6f01d157e3128ed9e54efcd6dbf16a9070b9ca90
poojakadolkar <kadolkarpooja129@gmail.com>
2024-06-17 22:23:39 +0530

    Update main by pooja.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 terraform {
-    required_version = "~> 1.0.3" // added latest version
+    required_version = "~> 1.8.5" // added latest version
     required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0"
+      version = "~> 5.53.0"
      }
     }
    }
@@ -13,7 +13,7 @@ provider "aws" {
         #profile = "default"
 	}
 
-resource "aws_instance" "Raminfra" {
+resource "aws_instance" "PoojaInfra" {
 	ami = "ami-0c2b8ca1dad447f8a" 
 	instance_type = "t2.micro"
 }
